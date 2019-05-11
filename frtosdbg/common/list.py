2ab8a9321326b5cfd0ab29ddf911c407199414a5
Jan Mazur <dingorth@gmail.com>
2019-05-11 22:17:03 +0200

    list inheritance

--- frtosdbg/common/list.py
+++ frtosdbg/common/list.py
@@ -1,17 +1,17 @@
 import gdb
 
-# moze tutaj chce ze structgen klase do listy a potem zrobic
-# dziedziczenie i w potomnej klasie __iter__
-
 from frtosdbg.structgen import get_class_by_typestr
 
 ListClass = get_class_by_typestr('List_t')
 
-class FreeRTOSList():
+class FreeRTOSList(ListClass):
 
     def __init__(self, lst, cast_type_str="void*"):
-        self.base = lst
+        # is this a correct way?
+        super().__init__(lst)
         self.cast_type = gdb.lookup_type(cast_type_str)
+        # why cant i use self.xListEnd
+        # self.end_marker = self.xListEnd
         self.end_marker = lst['xListEnd']
         self.head = self.end_marker['pxNext']  # ptr to item
 
